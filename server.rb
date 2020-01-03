f96afca879fb59ff6cb0291ab922661a597a05c4
Srikar Reddy Ramesh Kumar <srameshkumar@nyc.yearup.org>
2020-01-03 14:24:03 -0500

    Added login page, signup page also added the navbar

--- server.rb
+++ server.rb
@@ -3,3 +3,19 @@ require 'sinatra'
 get '/' do
   erb :home
 end
+
+get '/home' do
+  erb :home
+end
+
+get '/posts' do
+  erb :posts
+end
+
+get '/login' do
+  erb :login
+end
+
+get '/signup' do
+  erb :signup
+end
