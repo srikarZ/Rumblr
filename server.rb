88d7079317d1aaa06b02104e40c353bf206d1c59
Srikar <kalikisrikar@gmail.com>
2020-01-06 20:38:43 -0500

    Added several routes a few more remaining

--- server.rb
+++ server.rb
@@ -28,5 +28,57 @@ get '/signup' do
   erb :signup
 end
 
+post '/login' do
 
+  @user = Users.find_by(email: params[:email])
+  if @user && @user.password == params[:password]
+
+    session[:user_id] = @user.id
+
+    redirect "/profile"
+  else
+
+    flash[:warning] = "Your email or password is incorrect"
+
+
+    redirect "/"
+  end
+end
+
+post "/sign-up" do
+  @user = Users.create(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    birthday: params[:birthday],
+    email: params[:email],
+    password: params[:password],
+  )
+  @name = params[:first_name]
+
+  session[:user_id] = @user.id
+
+
+  redirect "/profile"
+end
+
+
+get "/logout" do
+
+  session[:user_id] = nil
+
+  redirect "/"
+end
+
+get "/profile" do
+
+  @user = User.find(session[:user_id])
+  @name = @user.username
+  @posts = @user.posts
+
+  erb :profile
+end
+
+# post "/profile" do
+#
+# end
 
